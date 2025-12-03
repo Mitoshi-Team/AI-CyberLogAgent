bed1696328b8def64d88df30d6bbd08e8f0d5d34
Вахрушев Матвей <132921axstiz@users.no-reply.gitverse.ru>
2025-12-03 21:55:44 +0000

    update test_cicd.py

--- test_cicd.py
+++ test_cicd.py
@@ -1,13 +1,15 @@
 qq = 12
 exec("print(8 )")
-a = set([1, 2]).add(18)
-
-
+a = set([1, 2]).add(18+78)
+q='ss'+'qq'
 def f(q):
     x = 13
     a = 12
     b = 11
     return q
 
-
 """Dich """
+
+s=f( 13)
+
+print(f(q)+ 13)
